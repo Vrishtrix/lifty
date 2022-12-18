7a53ed2f201dad1654d73ad28736c302d6a3f049
Vrishin Patel <39858602+Vrishtrix@users.noreply.github.com>
2022-12-18 10:39:35 +0000

    Added masterc ss (dependency)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import '@master/css';
+import '@master/normal.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
