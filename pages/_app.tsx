73281758a1e04daeab8a938087090d7cc91f40da
Vrishin Patel <39858602+Vrishtrix@users.noreply.github.com>
2022-12-30 06:35:57 +0000

    setup supabase helper for next (auth)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
 import '@master/css';
 import '@master/normal.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
-}
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
+import { useState } from 'react';
+
+import type { AppProps } from 'next/app';
+
+const MyApp = ({
+	Component,
+	pageProps,
+}: AppProps<{
+	initialSession: Session;
+}>) => {
+	const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+	return (
+		<SessionContextProvider
+			supabaseClient={supabaseClient}
+			initialSession={pageProps.initialSession}
+		>
+			<Component {...pageProps} />
+		</SessionContextProvider>
+	);
+};
+
+export default MyApp;
